1869eda838c0c6730535fdbe96f1df377085b4a3
Nikunj <nikunj.vadodariya@marutitech.com>
2025-10-07 09:14:28 +0530

    Add logic to handle the delimiters [\n | ,]]

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,6 +1,27 @@
+import re
+
+
 class NegativeNumberError(Exception):
     pass
 
 
-def add(numbers: str) -> int:
-    return -1
+def add(nums_str: str) -> int:
+    if not nums_str or nums_str.strip() == "":
+        return 0
+
+    delimiter = ",|\n"
+
+    numbers_str = re.split(delimiter, nums_str)
+    numbers = []
+    for num in numbers_str:
+        try:
+            numbers.append(int(num))
+        except:
+            pass
+    negatives = [n for n in numbers if n < 0]
+    if negatives:
+        raise NegativeNumberError(
+            f"negative numbers not allowed {','.join(map(str, negatives))}"
+        )
+
+    return sum(numbers)
